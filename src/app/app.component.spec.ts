e624467e3835eee8b825451b8a333fb6fb02d4eb
guiizis <guigamoda@hotmail.com>
2025-03-12 20:22:58 -0300

    rm intentional error

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -11,7 +11,7 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app).not.toBeTruthy();
+    expect(app).toBeTruthy();
   });
 
   it(`should have the 'test-angular' title`, () => {
